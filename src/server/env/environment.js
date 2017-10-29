0d2ff8d886c9031290139cba11b46bca1371d9a9
Jeroen van Lier <jeroen@hillstar.nl>
2017-10-29 15:31:44 +0100

    fixed bugs

--- src/server/env/environment.js
+++ src/server/env/environment.js
@@ -1,6 +1,6 @@
 const cosmosPort = 10255; // replace with your port
 const dbName = 'my-cosmos-heroed';
-const key = 'RNSBBmNFU3rHKZXnoXUGlAOTGwb0Yxf6WcJmomLU80mL7qoLYIK6ZoN5aV9kNHALq27bwvKPQtO7PFkY2NOwAg';
+const key = 'RNSBBmNFU3rHKZXnoXUGlAOTGwb0Yxf6WcJmomLU80mL7qoLYIK6ZoN5aV9kNHALq27bwvKPQtO7PFkY2NOwAg==';
 
 module.exports = {
   dbName,
